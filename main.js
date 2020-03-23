18ea5f071b6e11c37ffbedf1f07569e84ebd39ee
SCOTT <scott.kutler@gmail.com>
2020-03-23 15:43:13 -0400

    first draft with html / css

---
+++ main.js
@@ -0,0 +1,14 @@
+let allSquares = document.querySelectorAll(".square");
+let playerTurn1 = "Player 1";
+let playerTurn2 = "Player 2";
+
+/* possible winning combinations
+square1 == square2 == square3
+square4 == square5 == square6
+square7 == square8 == square9
+square1 == square4 == square7
+square2 == square5 == square8
+square3 == square6 == square9
+square1 == square5 == square9
+square3 == square5 == square7
+*/
