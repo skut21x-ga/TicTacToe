6eaff0b1fb117bdd118d7ffcb3d137d7d761d118
SCOTT <scott.kutler@gmail.com>
2020-03-24 03:12:08 -0400

    box select logic finished and working

--- main.js
+++ main.js
@@ -1,6 +1,46 @@
 let allSquares = document.querySelectorAll(".square");
-let playerTurn1 = "Player 1";
-let playerTurn2 = "Player 2";
+let playerTurn = document.querySelector(".playerIs");
+playerTurn.textContent = "Player 1";
+
+function squareClicked(evt) {
+  evt.preventDefault();
+  let square = evt.target;
+  if (playerTurn.value == 1 && square.value !== 1 && square.value !== 2) {
+    square.value = 1;
+    square.style.backgroundColor = "blue";
+    playerTurn.textContent = "Player 2";
+    console.log(
+      "clicked square value is " +
+        square.value +
+        " and was selected by Player" +
+        playerTurn.value
+    );
+    playerTurn.value = 2;
+  } else if (
+    playerTurn.value == 2 &&
+    square.value !== 1 &&
+    square.value !== 2
+  ) {
+    square.value = 2;
+    square.style.backgroundColor = "red";
+    playerTurn.textContent = "Player 1";
+    console.log(
+      "clicked square value is " +
+        square.value +
+        " and was selected by Player" +
+        playerTurn.value
+    );
+    playerTurn.value = 1;
+  } else if (square.value === 1 && playerTurn.value == 1) {
+    console.log("You already chose this box");
+  } else if (square.value === 1 && playerTurn.value == 2) {
+    console.log("You already chose this box");
+  } else if (square.value === 2 && playerTurn.value == 1) {
+    console.log("Player 2 already chose this box");
+  } else if (square.value === 1 && playerTurn.value == 2) {
+    console.log("You already chose this box");
+  } else square.style.backgroundColor = "pink";
+}
 
 /* possible winning combinations
 square1 == square2 == square3
@@ -12,3 +52,8 @@ square3 == square6 == square9
 square1 == square5 == square9
 square3 == square5 == square7
 */
+
+for (let i = 0; i < 9; i++) {
+  playerTurn.value = 1;
+  allSquares[i].addEventListener("click", squareClicked);
+}
